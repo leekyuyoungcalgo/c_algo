0c6301853a765e92312159d5ccc36b5360efb7d0
leekyuyoungcalgo <111040352+leekyuyoungcalgo@users.noreply.github.com>
2022-08-11 15:08:52 +0900

    Add files via upload

---
+++ 20220811/instack.h
@@ -0,0 +1,29 @@
+#ifndef ___instack
+#define ___instack
+#include<stdbool.h>
+typedef struct
+{
+	int max; // 최대 용량
+	int ptr;  // index 번호
+	int* stk;  // 스텍의 시작 지점(배열의 시작 주소 동적생성)
+}Instack;
+
+// 초기화
+int Initialize(Instack* s, int max);
+// push 데이터를 삽입
+int Push(Instack* s, int x);
+// pop 데이터를 가져옮
+int Pop(Instack* s, int* x);
+int Peek(const Instack* s, int* x);
+int Clear(Instack* s);
+int Capacity(const Instack* s);
+int Size(const Instack* s);
+int IsEmpty(const Instack* s);
+
+int IsFull(const Instack* s);
+int Search(const Instack* s, int x);
+
+bool isSearch(const Instack* s, int x, int* idx);
+void showAllStack(const Instack* s);
+int Terminate(Instack* s);
+#endif
